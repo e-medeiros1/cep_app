342741ce82dd19a52b52654f13ee9d7d5c446511
Medeiros <hsymetry@gmail.com>
2022-10-09 18:17:27 -0300

    first commit

---
+++ android/app/src/main/kotlin/br/com/erimedeiros/cep_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package br.com.erimedeiros.cep_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,10 @@
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+void main(List<String> args) {
+  runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home: null,
+  ));
+}
